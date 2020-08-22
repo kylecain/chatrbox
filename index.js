9ea44e6d0ea24fc10bb489129b294c2cd4a562e5
Kyle Cain <kylecain0305@gmail.com>
2020-08-21 23:46:00 -0500

    created simple webserver with error handling

---
+++ index.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var fs = require('fs');
+var extract = require('./extract');
+
+var handleError = function (err, res) {
+    res.writeHead(404);
+    res.end();
+}
+
+var server = http.createServer((req, res) => {
+    console.log('responding to a request');
+
+    var filePath = extract(req.url);
+    fs.readFile(filePath, (err, data) => {
+        if (err){
+            handleError(err, res);
+            return;
+        }else{
+        res.end(data);
+        }
+    });
+});
+
+server.listen(3000);
